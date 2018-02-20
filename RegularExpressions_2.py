7aaabe07243efaf79e70c4f7f0fedfec736094b2
A Williams <stixaw@gmail.com>
2018-02-20 12:11:24 -0700

    Add files via upload

---
+++ RegularExpressions_2.py
@@ -0,0 +1,68 @@
+''' Regular Expressions 2'''
+import re
+import os
+import sys
+
+
+#This is the main program
+if __name__ == '__main__':
+	
+	text = "aaabbbbbaaabbbbababbababbbbabcad"
+	
+	# followed by zero or more bs
+	print("using ab* repitition")
+	pat = re.compile(r'ab*')
+	m= re.search(pat, text)
+	m1 = re.findall(pat, text)
+	print(m.group(), m.span())
+	print(m1)
+	
+	# followed by one or more b
+	print("using ab+ repitition")
+	pat1 = re.compile(r'ab+')
+	m = re.search(pat1,text)
+	m1 = re.findall(pat1,text)
+	print(m.group(), m.span())
+	print(m1)
+	
+	# followed by zero or one b
+	print("using ab? repitition")
+	pat3 = re.compile(r'ab?')
+	m = re.search(pat3,text)
+	m1 = re.findall(pat3,text)
+	print(m.group(), m.span())
+	print(m1)
+	
+	# followed by n bs
+	print("using ab{n} repitition")
+	pat4 = re.compile(r'ab{2}')
+	m = re.search(pat4,text)
+	m1 = re.findall(pat4,text)
+	print(m.group(), m.span())
+	print(m1)
+	
+	# followed by min m and max n bs
+	print("using ab{m,n} repitition")
+	pat5 = re.compile(r'ab{1,3}')
+	m = re.search(pat5,text)
+	m1 = re.findall(pat5,text)
+	print(m.group(), m.span())
+	print(m1)
+	
+	# followd by min m, unlimited b
+	print("using ab{m,} repitition")
+	pat6 = re.compile(r'ab{1,}')
+	m = re.search(pat6,text)
+	m1 = re.findall(pat6,text)
+	print(m.group(), m.span())
+	print(m1)
+	
+	# followed by a single non newline character
+	print("using ab. repitition")
+	pat7 = re.compile(r'ab.')
+	m = re.search(pat7, text)
+	m1 = re.findall(pat7, text)
+	print(m.group(), m.span())
+	print(m1)
+	
+	
