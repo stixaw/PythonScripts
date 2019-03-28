b51fe376ed68fdb8854f402e2c6c468e2ca15b01
A Williams <stixaw@gmail.com>
2019-03-28 09:22:06 -0600

    Add files via upload

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
-age = int(raw_input("whatis your age? "))
-if input.isdigit(raw_input) continue
-else age = int(raw_input("this must be a number enter your age again: ")
+import os
+import sys
+
+
+def makeRange(n):
+	result = (range(1,n+1))
+	print(*result,sep="")
+
+def makeLexicon(x,y,z,n):
+	result = [[a,b,c] for a in range(x+1) for b in range (y+1) for c in range(z+1) if a+b+c != n]
+	print (result)
+	
+def runnerUp(arr):
+	result = sorted(set(arr))[-2]
+	print(result)
+	
+students = [['Harry', 37.21], [Berry, 37.21], ['Tina', 37.2], ['Akrti', 41], ['Harsh', 39]]
+
+
+
+if __name__ == '__main__':
